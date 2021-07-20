1e53af853f27eba0ed9d5cee62a6b6d971b3421e
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2021-07-20 12:06:47 +0100

    Handle differing plays
    For rock-paper-scissors. Doesn't yet handle draws.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 
-function play () {
-  return 1
+function play (player1, player2) {
+  if (player1 === 'rock' && player2 === 'scissors') {
+    return 1
+  } else if (player1 === 'scissors' && player2 === 'rock') {
+    return 2
+  } else if (player1 === 'paper' && player2 === 'scissors') {
+    return 2
+  } else if (player1 === 'scissors' && player2 === 'paper') {
+    return 1
+  } else if (player1 === 'rock' && player2 === 'paper') {
+    return 2
+  } else if (player1 === 'paper' && player2 === 'rock') {
+    return 1
+  }
 }
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -3,5 +3,16 @@ const { play } = require('./index.js')
 describe('rock paper scissors', () => {
   it('rock beats scissors', () => {
     expect(play('rock', 'scissors')).toEqual(1)
+    expect(play('scissors', 'rock')).toEqual(2)
+  })
+
+  it('scissors beats paper', () => {
+    expect(play('scissors', 'paper')).toEqual(1)
+    expect(play('paper', 'scissors')).toEqual(2)
+  })
+
+  it('paper beats rock', () => {
+    expect(play('paper', 'rock')).toEqual(1)
+    expect(play('rock', 'paper')).toEqual(2)
   })
 })
